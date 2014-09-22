4f2f2c8c7627d1ebca02b33affd90d3ea1f4faad
Schwa <jshmtthwclrk@users.noreply.github.com>
2014-09-22 13:16:27 -0400

    Create accordionscript.js

---
+++ accordionscript.js
@@ -0,0 +1,19 @@
+
+$(function() {
+    $('#accordion > li').hover(
+        function () {
+            var $this = $(this);
+            $this.stop().animate({'width':'800px'},500);
+            $('.heading',$this).stop(true,true).fadeOut();
+            $('.bgDescription',$this).stop(true,true).slideDown(500);
+            $('.description',$this).stop(true,true).fadeIn();
+        },
+        function () {
+            var $this = $(this);
+            $this.stop().animate({'width':'145px'},500);
+            $('.heading',$this).stop(true,true).fadeIn();
+            $('.description',$this).stop(true,true).fadeOut(500);
+            $('.bgDescription',$this).stop(true,true).slideUp(700);
+        }
+    );
+});
